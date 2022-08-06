dc4f09088bbcb2e5349c2261087c16f9d66ed70b
ala-bouassida <alabouassida865@gmail.com>
2022-08-06 15:26:02 +0100

    my first state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import"./App.css";
+import Add from './component/Add';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    todo: [
+     { 
+      id: Math.random(),
+      task: "learn state",
+      isDone: false 
+    },
+    { 
+      id: Math.random(),
+      task: "learn hooks",
+      isDone: false 
+    },
+    { 
+      id: Math.random(),
+      task: "learn redux",
+      isDone: false 
+    },
+    ],
+  };
+  handleDelete = (id) =>{
+    this.setState({todo: this.state.todo.filter((el)=>el.id !=id)})
+  };
+  handleCompleted =(id) =>{
+    this.setState({
+      todo: this.state.todo.map((el)=>
+    el.id === id ? { ...el, isDone: !el.isDone}: el
+      ),
+    });
+  };
+  render (){
+  console.log(this.state.todo);
+   
+  return(
+  <div style={{textAlign:"center"}}>
+    
+
+    {this.state.todo.map((el) =>(
+  <div id="div">
+    <p>{el.id}</p>
+    <h3 id={el.isDone? "completed": "nothing"}>{el.task}</h3>
+    <button onClick={()=>this.handleCompleted(el.id)}>isDone</button>
+    <button onClick={()=>this.handleDelete(el.id)}>delete</button>
+  </div>
+  ))}
+  </div>
+  )
+  };
+};
 
 export default App;
